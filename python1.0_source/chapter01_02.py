4a0c469107a4ffe2cccf387a23ee679d316dd26f
kcj4800 <chanjung4800@naver.com>
2023-03-27 11:20:41 +0900

    파이썬 입문 챕터1_2 - 환경설정

---
+++ python1.0_source/chapter01_02.py
@@ -0,0 +1,30 @@
+chapter 01_02 파이썬 기본 환경 설정
+
+1. 파이썬 개발 환경
+    - 파이썬 설치
+        google에 python 검색해서 최신버젼 windows installer (64 bit) 다운로드
+        윈도우 계정명이 한글명일 경우 영어로 바꾸는것을 추천!
+        관리자 권한으로 실행하여 설치! add python exe to path 체크하기!
+        cmd 관리자 권한으로 실행 후 python을 쳐서 버젼 확인
+
+    - 다양한 IDE 소개
+
+    - 파이썬 기본 IDE 실행
+
+2. 에디터 설치 및 실행
+    - VSCODE Editor 설치
+        google에 vscode 검색해서 추천해주는 stable build(안정화된 빌드버젼) 다운로드
+        관리자권한으로 실행 후 설치 path에 추가하기 체크!
+        cmd 관리자 권한으로 실행 후 code라고 쳐서 vscode 실행
+
+    - VSCODE Editor Plugin 설치
+
+    - 파이썬 연동 및 실행 테스트
+        extension에서 python 설치
+
+3. vscode 사용법
+    ctrl + : 화면 확대
+    ctrl - : 화면 축소
+    ctrl shift p : command palette -> python :select interpreter
+    ctrl f5 : run without debugging
+    
